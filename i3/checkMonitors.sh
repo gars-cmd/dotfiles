f5d0b52324c1fc33606e5b8a13eff19f135ffbaf
gars-cmd <avidouille@gmail.com>
2022-10-19 12:57:11 +0300

    update i3config

--- i3/checkMonitors.sh
+++ i3/checkMonitors.sh
@@ -10,7 +10,7 @@ echo $built_in
 echo $main_screen
 echo $vertical_screen
 # the nomade setup : no external screen 
-if [ $built_in == 1  -a $main_screen == 0 -a $vertical_screen==0 ]; then
+if [ $built_in == 1  -a $main_screen == 0 -a $vertical_screen == 0 ]; then
   xrandr --output eDP-1 --primary --auto
   echo "built_in screen"
 
